19fe6b3133d497b37d6393f79efb0cf8d35ee1c6
__b_2002 <natthaphong010245@gmail.com>
2025-08-01 00:01:39 +0700

    rch

--- resources/views/layouts/home/index.blade.php
+++ resources/views/layouts/home/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HOME</title>
+    <title>YOUTH CYBERSAFE</title>
     <link rel="icon" href="./images/logo.png">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=K2D:wght@400;600&display=swap" rel="stylesheet">
--- resources/views/layouts/index/index.blade.php
+++ resources/views/layouts/index/index.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Anti-Cyberbullying</title>
+  <title>YOUTH CYBERSAFE</title>
   <link rel="icon" href="./images/logo.png">
   <link href="https://fonts.googleapis.com/css2?family=K2D:wght@500&display=swap" rel="stylesheet">
   <script src="https://cdn.tailwindcss.com"></script>
